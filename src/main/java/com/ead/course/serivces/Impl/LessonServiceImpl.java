b82c70b32f9f485aa81aa7570ba3d0f7b5e8bae6
João Domingos <j13domingos@gmail.com>
2024-02-05 21:38:04 -0300

    feat: save new lesson

--- src/main/java/com/ead/course/serivces/Impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/serivces/Impl/LessonServiceImpl.java
@@ -3,6 +3,7 @@ package com.ead.course.serivces.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.serivces.LessonService;
 
@@ -11,4 +12,9 @@ public class LessonServiceImpl implements LessonService {
     
     @Autowired
     private LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
 }
--- src/main/java/com/ead/course/serivces/LessonService.java
+++ src/main/java/com/ead/course/serivces/LessonService.java
@@ -1,5 +1,9 @@
 package com.ead.course.serivces;
 
+import com.ead.course.models.LessonModel;
+
 public interface LessonService {
+
+    LessonModel save(LessonModel lessonModel);
     
 }
