85f5548f4aa30709d65689e6d223592d36fbc17d
João Domingos <j13domingos@gmail.com>
2024-02-05 21:36:11 -0300

    feat: add lessonDto

---
+++ src/main/java/com/ead/course/dtos/LessonDto.java
@@ -0,0 +1,14 @@
+package com.ead.course.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LessonDto {
+    
+    @NotBlank
+    private String title;
+    private String descrioption;
+    @NotBlank
+    private String videoUrl;
+}
