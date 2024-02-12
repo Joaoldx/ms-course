35a8e73618efc415839665ed9c30e99c43b1b4fa
João Domingos <j13domingos@gmail.com>
2024-02-11 22:43:13 -0300

    feat: update endpoint with specification criteria and pagiable

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -2,12 +2,16 @@ package com.ead.course.controllers;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,7 +28,7 @@ import com.ead.course.models.CourseModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.serivces.CourseService;
 import com.ead.course.serivces.ModuleService;
-
+import com.ead.course.specifications.SpecificationTemplate;
 
 import jakarta.validation.Valid;
 
@@ -90,8 +94,10 @@ public class ModuleController {
     }
     
     @GetMapping("/courses/{courseId}/modules")
-    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable(value = "courseId") UUID courseId) {
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(courseId));
+    public ResponseEntity<Page<ModuleModel>> getAllModules(@PathVariable(value = "courseId") UUID courseId,
+                                                            Specification<ModuleModel> spec, 
+                                                            @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
     }
     
     
