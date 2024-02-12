b77b9f9c953059f3c772d1db29791f38ae456533
João Domingos <j13domingos@gmail.com>
2024-02-11 22:42:06 -0300

    feat: add criteria query to search modules in course

--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -1,9 +1,16 @@
 package com.ead.course.specifications;
 
+import java.util.Collection;
+import java.util.UUID;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Root;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
@@ -17,4 +24,20 @@ public class SpecificationTemplate {
         @Spec(path = "name", spec = Like.class)
     })
     public interface CourseSpec extends Specification<CourseModel> {}
+
+    @Spec(path = "title", spec = Like.class)
+    public interface ModuleSpec extends Specification<ModuleModel> {}
+    
+    @Spec(path = "title", spec = Like.class)
+    public interface LessonSpec extends Specification<LessonModel> {}
+
+    public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<ModuleModel> module = root;
+            Root<CourseModel> course = query.from(CourseModel.class);
+            Expression<Collection<ModuleModel>> coursesModules = course.get("modules");
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, coursesModules));
+        };
+    }
 }
