2cc65ecfc0c289a4e9520871c2b84b3137cca375
João Domingos <j13domingos@gmail.com>
2024-02-18 22:31:01 -0300

    feat: create request to another micro service

---
+++ src/main/java/com/ead/course/clients/CourseClient.java
@@ -0,0 +1,57 @@
+package com.ead.course.clients;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import com.ead.course.dtos.ResponsePageDto;
+import com.ead.course.dtos.UserDto;
+import com.ead.course.serivces.UtilsService;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class CourseClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    UtilsService utilsService;
+
+    public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable) {
+        
+        List<UserDto> searchResult = null;
+        
+        String url = utilsService.createUrl(courseId, pageable);
+    
+        ResponseEntity<ResponsePageDto<UserDto>> result = null;
+
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+       
+        try {
+            ParameterizedTypeReference<ResponsePageDto<UserDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<UserDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+
+            log.debug("Response Number of Elements: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e) {
+            log.debug("Error request /users {} ", e);
+        }
+
+        log.info("Ending request /users userId {} ", courseId);
+        return result.getBody();
+    }
+
+}
