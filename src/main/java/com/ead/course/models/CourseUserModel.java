7a92760df6fede16e4bcbc14b7630266ddcc7546
João Domingos <j13domingos@gmail.com>
2024-02-22 20:13:45 -0300

    refactor: change property order

--- src/main/java/com/ead/course/models/CourseUserModel.java
+++ src/main/java/com/ead/course/models/CourseUserModel.java
@@ -13,9 +13,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_COURSES_USERS")
@@ -26,9 +30,9 @@ public class CourseUserModel implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private CourseModel course;
-    
     @Column(nullable = false)
     private UUID userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private CourseModel course;
 }
