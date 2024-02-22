63026d0575967453f761ee5dc8e70235503fc0aa
João Domingos <j13domingos@gmail.com>
2024-02-21 21:52:01 -0300

    fix: add correct repository

--- src/main/java/com/ead/course/serivces/Impl/CourseUserServiceImpl.java
+++ src/main/java/com/ead/course/serivces/Impl/CourseUserServiceImpl.java
@@ -3,12 +3,12 @@ package com.ead.course.serivces.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.CourseUserRepository;
 import com.ead.course.serivces.CourseUserService;
 
 @Service
 public class CourseUserServiceImpl implements CourseUserService {
     
     @Autowired
-    CourseRepository courseRepository;
+    CourseUserRepository courseUserRepository;
 }
